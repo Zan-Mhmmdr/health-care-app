fd99543d77f760b3c4ca468484fc82de873c6bb3
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-05-05 21:20:52 +0700

    feat: integrate to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,14 +14,19 @@ const geistMono = Geist_Mono({
 
 export default function RootLayout({
   children,
+  MainPage,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
+  MainPage: React.ReactNode
 }) {
 
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased `}>
         <main>{children}</main>
+        <div>
+          {MainPage}
+        </div>
       </body>
     </html>
   )
