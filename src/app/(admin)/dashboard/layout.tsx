1f49097df973ddab51c34c7f1dd2aa13d3e9f92f
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-05-07 13:44:06 +0700

    refactor: dashboard page

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -10,10 +10,10 @@ export default function DashboardLayout({
     payments: React.ReactNode
 }) {
     return (
-        <main>
-            <div>{children}</div>
-            <div>{products}{analytics}</div>
-            <div>{payments}</div>
+        <main className="flex flex-col justify-center items-center">
+            <div className="w-[1400] mx-5 mt-5 h-96 bg-slate-500 text-white flex items-center justify-center">{children}</div>
+            <div className="flex flex-row m-5 w-[1400] h-96 justify-between items-center">{products}{analytics}</div>
+            <div className="w-[1400] mx-5 my-5 h-96 bg-slate-500 text-white flex items-center justify-center">{payments}</div>
         </main>
     );
 }
