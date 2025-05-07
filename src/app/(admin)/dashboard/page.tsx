e76872e68992a4ff84d3b62145b394e5255bb7b9
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-05-07 15:15:46 +0700

    refactor: css in dashboard page

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 export default function Dashboard() {
     return (
-        <div className="flex flex-col gap-4 items-center justify-center ">
+        <div className="flex flex-col gap-4 items-center justify-center  ">
             <h1 className="text-3xl font-bold" >Dashboard</h1>
             <p className="text-gray-900 text-xl ">Welcome to the admin dashboard!</p>
         </div>
