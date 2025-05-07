a1d41e5f0259587235b0d5cc2747b57909cb403b
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-05-07 15:15:46 +0700

    refactor: css in dashboard page

--- src/app/(admin)/dashboard/@products/page.tsx
+++ src/app/(admin)/dashboard/@products/page.tsx
@@ -1,4 +1,4 @@
 export default function ProductsPage() {
-    return <div className="bg-slate-500 w-[650] h-80 flex items-center justify-center text-xl text-white">Products</div>;
+    return <div className="bg-slate-500 w-[650] h-80 flex items-center justify-center rounded-lg text-xl text-white">Products</div>;
   }
   
--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -11,9 +11,9 @@ export default function DashboardLayout({
 }) {
     return (
         <main className="flex flex-col justify-center items-center">
-            <div className="w-[1400] mx-5 mt-5 h-96 bg-slate-500 text-white flex items-center justify-center">{children}</div>
+            <div className="w-[1400] mx-5 mt-5 h-96 bg-slate-500 text-white flex items-center justify-center rounded-lg">{children}</div>
             <div className="flex flex-row m-5 w-[1400] h-96 justify-between items-center">{products}{analytics}</div>
-            <div className="w-[1400] mx-5 my-5 h-96 bg-slate-500 text-white flex items-center justify-center">{payments}</div>
+            <div className="w-[1400] mx-5 my-5 h-96 bg-slate-500 text-white flex items-center justify-center rounded-lg">{payments}</div>
         </main>
     );
 }
