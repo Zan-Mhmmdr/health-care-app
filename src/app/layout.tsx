e8eac8e0b9951fb2156a44cd663201d46a93a347
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-05-07 14:37:20 +0700

    refactor: organize page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+import Header from "@/components/Header/Header"
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -23,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased `}>
+        <Header/>
         <main>
           {children}
           <div>
