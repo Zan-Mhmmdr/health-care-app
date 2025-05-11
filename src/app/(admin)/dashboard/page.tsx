06277a86d208826ddbb8fbf9b300838291d54cda
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-05-11 19:16:23 +0700

    feat: add next-auth

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,4 +1,19 @@
+'use client'
+
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+
 export default function Dashboard() {
+    const { data: session, status } = useSession()
+    const router = useRouter()
+
+    useEffect(() => {
+        if (status === "unauthenticated") {
+            router.push("/login")
+        }
+    }, [status, router])
+
     return (
         <div className="flex flex-col gap-4 items-center justify-center  ">
             <h1 className="text-3xl font-bold" >Dashboard</h1>
@@ -6,4 +21,3 @@ export default function Dashboard() {
         </div>
     )
 }
-
