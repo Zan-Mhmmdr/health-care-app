2cbcdc251a580f01c3eb06775443a24c8a77854c
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-06-14 21:38:34 +0700

    update: page parallel route

--- src/app/(admin)/dashboard/@products/page.tsx
+++ src/app/(admin)/dashboard/@products/page.tsx
@@ -1,4 +1,9 @@
 export default function ProductsPage() {
-    return <div className="bg-slate-500 w-[650] h-80 flex items-center justify-center rounded-lg text-xl text-white">Products</div>;
-  }
-  
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+      <div className="bg-slate-500 w-[650px] h-80 flex items-center justify-center rounded-2xl text-2xl text-white shadow-lg transition hover:scale-105 duration-300">
+        Products
+      </div>
+    </div>
+  );
+}
