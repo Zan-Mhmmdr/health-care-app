033fb808ca3ce2f87d033151d7f5135b2c77ad80
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-06-15 13:47:58 +0700

    update: page parallel route payments

--- src/app/(admin)/dashboard/@payments/page.tsx
+++ src/app/(admin)/dashboard/@payments/page.tsx
@@ -1,4 +1,9 @@
 export default function PaymentsPage() {
-    return <div className="">Payments</div>;
-  }
-  
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+      <div className="bg-indigo-500 w-[650px] h-80 flex items-center justify-center rounded-2xl text-2xl text-white shadow-lg transition hover:scale-105 duration-300">
+        Payments
+      </div>
+    </div>
+  );
+}
